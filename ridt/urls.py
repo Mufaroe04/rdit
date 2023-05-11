175f464945c340ecd3d04b00eb3aee82f697e82b
Mufaroe04 <mufaromanyama@gmail.com>
2023-05-11 13:03:32 +0200

    profile  commit

--- ridt/urls.py
+++ ridt/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('website/',include('website.urls')),
     # path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
